c25840e221ba39060aee7eff5129c0f08b285117
Giovanni Visciano <giovanni.visciano@statpro.com>
2020-06-09 13:33:30 +0200

    ingredients list

--- src/components/IngredientsPercentage.tsx
+++ src/components/IngredientsPercentage.tsx
@@ -1,22 +1,31 @@
 import React from "react";
-import { IonList, IonItem, IonLabel, IonListHeader } from "@ionic/react";
+import { IonList, IonItem, IonLabel, IonListHeader, IonInput, IonText } from "@ionic/react";
 import { Ingredient } from "./Recipe";
 import "./IngredientsPercentage.css";
 
 interface Props {
   title: string;
   ingredients: Ingredient[];
+  maxPercentage?: number;
 }
 
-const IngredientsPercentage: React.FC<Props> = ({ title, ingredients }) => {
+const IngredientsPercentage: React.FC<Props> = ({ title, ingredients, maxPercentage }) => {
   return (
     <IonList lines="none">
-      <IonListHeader lines="inset">
+      <IonListHeader lines="inset" className="ion-no-padding">
         <IonLabel className="title ion-text-center">{title}</IonLabel>
       </IonListHeader>
       {ingredients.map((ingredient) => (
-        <IonItem>
+        <IonItem key={ingredient.name}>
           <IonLabel>{ingredient.name}</IonLabel>
+          <IonInput
+            className="ion-text-right"
+            value={ingredient.value}
+            type="number"
+            min="0"
+            max={maxPercentage?.toString()}
+          ></IonInput>
+          <IonText class="ion-padding-start">%</IonText>
         </IonItem>
       ))}
     </IonList>
