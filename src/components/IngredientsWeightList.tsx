a55c8cd4257434efb3c5049c9488cd21f52926cc
Giovanni Visciano <giovanni.visciano@statpro.com>
2020-06-25 21:08:44 +0200

    fmt

--- src/components/IngredientsWeightList.tsx
+++ src/components/IngredientsWeightList.tsx
@@ -28,7 +28,9 @@ const IngredientsWeightList: React.FC<Props> = ({ title, ingredientsPercentage, 
         {[...ingredientsPercentage.entries()].map(([name, percentage], idx) => (
           <IonRow key={`${idx}-${name}`} className={idx % 2 === 0 ? "background-light" : undefined}>
             <IonCol className="ion-text-start">{name}</IonCol>
-            <IonCol size="2" className="ion-text-end">{percentage}</IonCol>
+            <IonCol size="2" className="ion-text-end">
+              {percentage}
+            </IonCol>
             <IonCol className="ion-text-end">{ingredientsWeight.get(name)!.toFixed(2)}</IonCol>
           </IonRow>
         ))}
