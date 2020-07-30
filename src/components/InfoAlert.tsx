217e2f4a7bc4caa4e6df7a79c230fc3d93838a19
Giovanni Visciano <giovanni.visciano@statpro.com>
2020-07-30 09:23:19 +0200

    assets

--- src/components/InfoAlert.tsx
+++ src/components/InfoAlert.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { IonAlert } from "@ionic/react";
+import donate from "assets/img/donate.gif";
 
 type Props = {
   showAlert: boolean;
@@ -8,19 +9,21 @@ type Props = {
 
 let InfoAlert: React.FC<Props> = ({ showAlert, setShowAlert }) => {
   const message = `
-    <p>
-      <a href="mailto:panificazionefavaglie@gmail.com?subject=BakerCalc">
-        panificazionefavaglie@gmail.com
-      </a>
-    </p>
-    <p style="text-align: center;">
-      If you like the App<br/>support me with a beer!<br/>Thank you
-    </p>
-    <p style="width: auto; text-align: center;">
-      <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=YWVFRGAC55UX4">
-      <img src="/assets/icon/donate.gif"/>
-      </a>
-    </p>
+    <div class="ion-text-center">
+      <p>
+        <a href="mailto:panificazionefavaglie@gmail.com?subject=BakerCalc">
+          Info mail
+        </a>
+      </p>
+      <p>
+        If you like the App<br/>support me with a beer!<br/>Thank you
+      </p>
+      <p>
+        <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=YWVFRGAC55UX4">
+        <img src=${donate}>
+        </a>
+      </p>
+    </div>
   `;
 
   return <IonAlert isOpen={showAlert} onDidDismiss={() => setShowAlert(false)} header={"Info"} message={message} />;
