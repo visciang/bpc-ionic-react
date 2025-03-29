d4b25655b21ad76baf6e49d2e75136f9e3c491e7
visciang <visciang@users.noreply.github.com>
2025-03-29 19:59:04 +0000

    deploy: 0295ecd24a2949461a9252dc52cd603542277d48

--- assets/swipe-back-legacy-CXGK2kCi.js
+++ assets/swipe-back-legacy-CXGK2kCi.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-JUywwf3M.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-UXp7lHXP.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
