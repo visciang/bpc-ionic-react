003e37871f88092002fa3c5ebefdc53b0405a684
visciang <visciang@users.noreply.github.com>
2025-03-30 17:27:20 +0000

    deploy: b27719cdba92b58f90ab61ccc8a25994f50b7e99

--- assets/ios.transition-legacy-hsaSN4bX.js
+++ assets/ios.transition-legacy-hsaSN4bX.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-AtX0xrPW.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-C0Y0snxW.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
