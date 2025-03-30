aae5bead3b6a9109373ee92d5bbe06e4b489874e
visciang <visciang@users.noreply.github.com>
2025-03-30 20:49:55 +0000

    deploy: 18876bdafde581e6889fc8fc5bf2dccdf91fdd5f

--- assets/ios.transition-legacy-C1ZN6Uh_.js
+++ assets/ios.transition-legacy-C1ZN6Uh_.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C7veIBDS.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-C8z9dYQa.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-Ds7HzOwu.js
+++ assets/swipe-back-legacy-Ds7HzOwu.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C7veIBDS.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-C8z9dYQa.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
