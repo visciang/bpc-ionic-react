787d5a4d96c512b1f8b7427842a36fcbf07f4bda
visciang <visciang@users.noreply.github.com>
2025-04-02 19:42:32 +0000

    deploy: da27db8deb2f7502b49a17eaca30b2483d1b94dd

--- assets/status-tap-legacy-fYNUShhq.js
+++ assets/status-tap-legacy-fYNUShhq.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-8VmGUS7Y.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-44Srjm_Q.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-S3OSCxPU.js
+++ assets/swipe-back-legacy-S3OSCxPU.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-8VmGUS7Y.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-44Srjm_Q.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
