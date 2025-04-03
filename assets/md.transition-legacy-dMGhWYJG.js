eebe3ef99f56a5a0606073c221962b7b06b16ac0
visciang <visciang@users.noreply.github.com>
2025-04-03 19:54:09 +0000

    deploy: 3ef7e4cfeb81f81c85bb8145e12748b6ea8647ac

--- assets/md.transition-legacy-dMGhWYJG.js
+++ assets/md.transition-legacy-dMGhWYJG.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-44Srjm_Q.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-C9IQcH8w.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
