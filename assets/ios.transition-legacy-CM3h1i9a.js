7a9a2dd07621b45168200f08ac0bd852c97ebed1
visciang <visciang@users.noreply.github.com>
2025-04-08 06:46:02 +0000

    deploy: 58dd69132b479a2e111155b04c62f90d926c5357

--- assets/ios.transition-legacy-CM3h1i9a.js
+++ assets/ios.transition-legacy-CM3h1i9a.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DMk8JRsZ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CNIehPPd.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
