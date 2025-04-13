bcd6424bcb73a5540cb9176269048eac39564902
visciang <visciang@users.noreply.github.com>
2025-04-13 07:52:26 +0000

    deploy: e8b17d023b0f275943b7d9d6b132f6a545dfae18

--- assets/md.transition-legacy-CHQ1dBtR.js
+++ assets/md.transition-legacy-CHQ1dBtR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DS9yayH2.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-CCXum-Ft.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
