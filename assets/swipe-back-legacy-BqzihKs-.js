b34b4ac0738daee93ed3a665f219ca7de1285be3
visciang <visciang@users.noreply.github.com>
2025-04-14 18:13:38 +0000

    deploy: cd9a55d7e00d82f7cc5dd1cf2ada3a48c3d68a03

--- assets/swipe-back-legacy-BqzihKs-.js
+++ assets/swipe-back-legacy-BqzihKs-.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CCXum-Ft.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-1-LyX65y.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
