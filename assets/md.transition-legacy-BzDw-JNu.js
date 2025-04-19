a44fb58c6a3fcf260469e42bd41fc6a2e27595de
visciang <visciang@users.noreply.github.com>
2025-04-19 16:19:24 +0000

    deploy: e765c496f4e050821ad5aae20394754093e64cd7

--- assets/md.transition-legacy-BzDw-JNu.js
+++ assets/md.transition-legacy-BzDw-JNu.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-1-LyX65y.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BsftZ5G5.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
